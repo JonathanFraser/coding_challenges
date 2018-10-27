4a01d9cda3255c1586a6597a7b0a730c65812b1a
Jonathan Fraser <jonathanfras@gmail.com>
2018-10-27 12:50:14 -0700

    got snake game framework compiling

--- challenges/SnakeGame.hs
+++ challenges/SnakeGame.hs
@@ -1,19 +1,30 @@
 import Graphics.Gloss 
-import Graphics.Gloss.Interface.IO.Play
+import Graphics.Gloss.Interface.IO.Game
 
 width = 400
-height = 400 
+height = 400
 
 data Snake = Snake [Point]
 
-drawBlock :: Point -> Picture 
-drawBlock pnt
+data Dir = Up | Down | Left | Right
+data Game = Game Snake Dir Point
 
+drawBlock :: Point -> Picture 
+drawBlock (x,y) = translate (10*x) (10*y) $ rectangleSolid  10 10 
+
+drawSnake :: Snake -> Picture
+drawSnake (Snake pts) = color white $ pictures $ fmap drawBlock pts 
+
+drawGame :: Game -> Picture 
+drawGame (Game snk _ pt) = pictures [color red $ drawBlock pt,drawSnake snk]
+
+updateGame :: Float -> Game -> IO Game 
+updateGame _ (Game snk dir pt) = return $ Game snk dir pt
 
 disp :: Display
 disp = InWindow "Snake Game" (width,height) (10,10)
 
 main :: IO ()
-main = do 
-    intial <-  getStdRandom (generateN 100)
-    playIO disp black 100 intial drawIO updateIO
+main = playIO disp black 100 intital (return.drawGame) (\_ g -> return g) updateGame
+                                where 
+                                    intital = Game (Snake [(3,3),(4,3),(4,4)]) Main.Up (5,5)
